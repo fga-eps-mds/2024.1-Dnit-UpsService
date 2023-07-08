34877a2f81381e97062da33217e95c045f00513d
Yan Luca <yanlucaviana@gmail.com>
2023-07-08 16:00:00 -0300

    #3 Alterando mensagem de erro planilha com dados errados
    Co-authored-by: migueldefrias <miguelfrias8@gmail.com>

--- service/SinistroService.cs
+++ service/SinistroService.cs
@@ -21,7 +21,7 @@ namespace service
         {
             using (var reader = new StreamReader(planilha))
             {
-                int tamanho_max = 1000;
+                int tamanho_max = 5000;
                 int quantidade_sinistros = -1;
 
                 while (reader.ReadLine() != null) { quantidade_sinistros++; }
@@ -44,34 +44,43 @@ namespace service
 
                     bool primeiralinha = false;
 
+
                     while (!parser.EndOfData)
                     {
-                        string[] linha = parser.ReadFields();
-                        if (!primeiralinha)
+                        try
                         {
-                            primeiralinha = true;
-                            continue;
-                        }
+                            string[] linha = parser.ReadFields();
+                            if (!primeiralinha)
+                            {
+                                primeiralinha = true;
+                                continue;
+                            }
 
-                        Sinistro sinistro = new Sinistro();
-                        sinistro.IdSinistro = int.Parse(linha[0]);
-                        sinistro.SiglaUF = linha[1];
-                        sinistro.Rodovia = int.Parse(linha[2]);
-                        sinistro.Km = double.Parse(linha[3]);
-                        sinistro.Snv = linha[4];
-                        sinistro.Sentido = linha[5];
-                        sinistro.Solo = linha[6];
-                        sinistro.Data = DateTime.Parse(linha[7]);
-                        sinistro.Tipo = linha[8];
-                        sinistro.Causa = linha[9];
-                        sinistro.Gravidade = linha[10];
-                        sinistro.Feridos = int.Parse(linha[11]);
-                        sinistro.Mortos = int.Parse(linha[12]);
-                        sinistro.Latitude = double.Parse(linha[13]);
-                        sinistro.Longitude = double.Parse(linha[14]);
-                        sinistro.CalcularUps();
-                        sinistroRepositorio.CadastrarSinistro(sinistro);
-                        numero_linha++;
+                            Sinistro sinistro = new Sinistro();
+                            sinistro.IdSinistro = int.Parse(linha[0]);
+                            sinistro.SiglaUF = linha[1];
+                            sinistro.Rodovia = int.Parse(linha[2]);
+                            sinistro.Km = double.Parse(linha[3]);
+                            sinistro.Snv = linha[4];
+                            sinistro.Sentido = linha[5];
+                            sinistro.Solo = linha[6];
+                            sinistro.Data = DateTime.Parse(linha[7]);
+                            sinistro.Tipo = linha[8];
+                            sinistro.Causa = linha[9];
+                            sinistro.Gravidade = linha[10];
+                            sinistro.Feridos = int.Parse(linha[11]);
+                            sinistro.Mortos = int.Parse(linha[12]);
+                            sinistro.Latitude = double.Parse(linha[13]);
+                            sinistro.Longitude = double.Parse(linha[14]);
+                            sinistro.CalcularUps();
+                            sinistroRepositorio.CadastrarSinistro(sinistro);
+                            numero_linha++;
+                        
+                        }
+                        catch(FormatException ex)
+                        {
+                            throw new Exception("Planilha com formato incompatível.");
+                        }
                     }
                 }
             }
