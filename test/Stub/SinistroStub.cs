bf2c069a04e5f3f2f5645e356bbca690f40c1082
Denys Rógeres <dennysrogeres@gmail.com>
2023-07-09 22:10:04 -0300

    removendo arquivo desnecessario

--- test/Stub/SinistroStub.cs
+++ test/Stub/SinistroStub.cs
@@ -1,12 +1,4 @@
 using dominio;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using dominio;
-using System;
 
 namespace test.Stub
 {
